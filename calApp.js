499bc4202d2cde1a711de495901eea680a520812
Vuong Kieu Linh <vuongkieulinh@Vuongs-MacBook-Air.local>
2021-08-26 06:09:00 +0700

    js code

---
+++ calApp.js
@@ -0,0 +1,13 @@
+const gridItem = document.querySelectorAll('.grid-item');
+const number = document.querySelector('.number1');
+const cal = document.querySelectorAll('.cal');
+const del = document.querySelector('#del');
+const reset = document.querySelector('#reset');
+const calculating = document.querySelector('#calculating');
+const result = document.querySelector('#result');
+
+gridItem.addEventListener('click', function() {
+    alert("clicked");
+})
+
+
