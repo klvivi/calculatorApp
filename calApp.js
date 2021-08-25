94036a59c89bfb16870baad8c0abcdbe6726c7e7
toanlc01 <congtoanle01@gmail.com>
2021-08-25 16:49:59 -0700

    update

--- calApp.js
+++ calApp.js
@@ -1,13 +1,19 @@
-const gridItem = document.querySelectorAll('.grid-item');
-const number = document.querySelector('.number1');
-const cal = document.querySelectorAll('.cal');
-const del = document.querySelector('#del');
-const reset = document.querySelector('#reset');
-const calculating = document.querySelector('#calculating');
-const result = document.querySelector('#result');
+// const gridItem = document.querySelectorAll('.grid-item');
+// const number = document.querySelector('.number1');
+// const cal = document.querySelectorAll('.cal');
+// const del = document.querySelector('#del');
+// const reset = document.querySelector('#reset');
+// const calculating = document.querySelector('#calculating');
+// const result = document.querySelector('#result');
 
-gridItem.addEventListener('click', function() {
-    alert("clicked");
-})
+// gridItem.addEventListener('click', function() {
+//     alert("clicked");
+// })
 
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('7').addEventListener('click', () => {
+        document.querySelector('#demo').innerHTML = '7';
+    }
+    )})
